5329a29841b1056c7ddd8c721e4f0b1a14cf2548
LeudiX <98977804+LeudiX@users.noreply.github.com>
2024-06-13 21:19:26 -0600

    Added some sticky social networks, some quotes over footer and go to up button style changed

--- js/typed.js
+++ js/typed.js
@@ -5,3 +5,16 @@ let typingText = new Typed("#text", {
     backDelay : 1500,
     loop : true,
 })
+
+let typingText2 = new Typed("#quotes", {
+    strings : [
+        "There are three responses to a piece of design – yes, no, and WOW! Wow is the one to aim for✨--Milton Glaser",
+        "Websites promote you 24/7: No employee will do that🪄--Paul Cookson",
+        "Getting a quality website is not an expense but rather an investment👨---Dr. Christopher Dayagdag",
+        "Digital design is like painting, except the paint never dries🚀---Neville Brody",
+    ],
+    typeSpeed : 50,
+    backSpeed : 50,
+    backDelay : 10000,
+    loop : true,
+})
