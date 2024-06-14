a6163bce35de679bc0ab596cf8e14ef8f41dc073
LeudiX <98977804+LeudiX@users.noreply.github.com>
2024-06-13 19:30:21 -0600

    Added some cool animations to hero section and text intro. Added contact list of social networks on left side, increased line heigth on subtitles groups

---
+++ js/typed.js
@@ -0,0 +1,7 @@
+let typingText = new Typed("#text", {
+    strings : ["Hi, I'm LeudiX✨","I'm a Computer Sciences Engineer🪄","I teach people!!!👨","I code stuffs...🚀","I'm a Tech Lover💓"],
+    typeSpeed : 50,
+    backSpeed : 50,
+    backDelay : 1500,
+    loop : true,
+})
